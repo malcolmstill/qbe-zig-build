92343c683bb5f8468065a6529eec3d93b0a74316
Malcolm Still <malcolm.still@gmail.com>
2025-01-04 17:47:00 +0000

    Update upstream qbe version + rearrange repo
    This updates to the latest master qbe. The nice thing that has happened
    since the initial version pulled in to this repo is that they have fixed
    the `asm` field name that was causing issues when compiling with zig (it
    is now `fmt`).
    
    As a consequence we no longer have to manually modify the original source
    to allow building with zig. This gets us very close to not having to pull
    in the upstream repo at all (where we could instead fetch it as part of
    build.zig.zon). However, the zig build system does seem able to depend
    on cgit repos at the moment and the current release tarball is from about
    a year ago...therefore we still for the moment need to pull the code in.
    
    Based on previous releases we might get a new version in February, at which
    point this repo can be mostly empty. Additionally, if the zig build system
    is updated to support cgit, or indeed cgit is updated so that it is compatible
    with zig, then we can have a version of this repo that keeps up with the
    upstream master without manually pulling in the code.

--- build.zig
+++ build.zig
@@ -43,40 +43,41 @@ pub fn build(b: *std.Build) !void {
 
     qbe_exe.addCSourceFiles(.{
         .files = &.{
-            "abi.c",
-            "alias.c",
-            "cfg.c",
-            "copy.c",
-            "emit.c",
-            "fold.c",
-            "live.c",
-            "load.c",
-            "main.c",
-            "mem.c",
-            "parse.c",
-            "rega.c",
-            "simpl.c",
-            "spill.c",
-            "ssa.c",
-            "util.c",
+            "qbe/abi.c",
+            "qbe/alias.c",
+            "qbe/cfg.c",
+            "qbe/copy.c",
+            "qbe/emit.c",
+            "qbe/fold.c",
+            "qbe/live.c",
+            "qbe/load.c",
+            "qbe/main.c",
+            "qbe/mem.c",
+            "qbe/parse.c",
+            "qbe/rega.c",
+            "qbe/simpl.c",
+            "qbe/spill.c",
+            "qbe/ssa.c",
+            "qbe/util.c",
             // amd64
-            "amd64/emit.c",
-            "amd64/isel.c",
-            "amd64/sysv.c",
-            "amd64/targ.c",
+            "qbe/amd64/emit.c",
+            "qbe/amd64/isel.c",
+            "qbe/amd64/sysv.c",
+            "qbe/amd64/targ.c",
             // arm64
-            "arm64/abi.c",
-            "arm64/emit.c",
-            "arm64/isel.c",
-            "arm64/targ.c",
+            "qbe/arm64/abi.c",
+            "qbe/arm64/emit.c",
+            "qbe/arm64/isel.c",
+            "qbe/arm64/targ.c",
             // rv64
-            "rv64/abi.c",
-            "rv64/emit.c",
-            "rv64/isel.c",
-            "rv64/targ.c",
+            "qbe/rv64/abi.c",
+            "qbe/rv64/emit.c",
+            "qbe/rv64/isel.c",
+            "qbe/rv64/targ.c",
         },
     });
 
+    qbe_exe.addIncludePath(b.path("qbe/"));
     qbe_exe.addIncludePath(wf.getDirectory());
 
     b.installArtifact(qbe_exe);
@@ -94,47 +95,49 @@ pub fn build(b: *std.Build) !void {
 
     libqbe.addCSourceFiles(.{
         .files = &.{
-            "abi.c",
-            "alias.c",
-            "cfg.c",
-            "copy.c",
-            "emit.c",
-            "fold.c",
-            "live.c",
-            "load.c",
-            "lib.c", // lib.c instead of main.c
-            "mem.c",
-            "parse.c",
-            "rega.c",
-            "simpl.c",
-            "spill.c",
-            "ssa.c",
-            "util.c",
+            "qbe/abi.c",
+            "qbe/alias.c",
+            "qbe/cfg.c",
+            "qbe/copy.c",
+            "qbe/emit.c",
+            "qbe/fold.c",
+            "qbe/live.c",
+            "qbe/load.c",
+            "src/lib.c", // lib.c instead of main.c
+            "qbe/mem.c",
+            "qbe/parse.c",
+            "qbe/rega.c",
+            "qbe/simpl.c",
+            "qbe/spill.c",
+            "qbe/ssa.c",
+            "qbe/util.c",
             // amd64
-            "amd64/emit.c",
-            "amd64/isel.c",
-            "amd64/sysv.c",
-            "amd64/targ.c",
+            "qbe/amd64/emit.c",
+            "qbe/amd64/isel.c",
+            "qbe/amd64/sysv.c",
+            "qbe/amd64/targ.c",
             // arm64
-            "arm64/abi.c",
-            "arm64/emit.c",
-            "arm64/isel.c",
-            "arm64/targ.c",
+            "qbe/arm64/abi.c",
+            "qbe/arm64/emit.c",
+            "qbe/arm64/isel.c",
+            "qbe/arm64/targ.c",
             // rv64
-            "rv64/abi.c",
-            "rv64/emit.c",
-            "rv64/isel.c",
-            "rv64/targ.c",
+            "qbe/rv64/abi.c",
+            "qbe/rv64/emit.c",
+            "qbe/rv64/isel.c",
+            "qbe/rv64/targ.c",
         },
     });
 
+    libqbe.addIncludePath(b.path("qbe/"));
     libqbe.addIncludePath(wf.getDirectory());
 
     const module = b.addModule("qbe-zig", .{
         .root_source_file = b.path("src/qbe.zig"),
     });
 
-    module.addIncludePath(b.path(""));
+    // src/qbe.zig needs to be able to find src/lib.h
+    module.addIncludePath(b.path("src"));
 
     b.installArtifact(libqbe);
 }
